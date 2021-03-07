59dff8ed4baeedef66e20cd4d3db9d87ab1f2c8e
Lucas Perazzo <lucas.perazzo27@gmail.com>
2021-03-07 19:59:54 -0300

    comentarios de paquete

--- frontend/src/redux/actions/paqueteActions.js
+++ frontend/src/redux/actions/paqueteActions.js
@@ -1,21 +1,22 @@
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const paqueteActions = {
   obtenerTodosLosPaquetes: () => {
     return async (dispatch, getState) => {
       try {
         const response = await axios.get('http://localhost:4000/api/paquetes')
-        dispatch({type: 'TODOS_PAQUETES', payload: response.data.response})
-    
+        dispatch({ type: 'TODOS_PAQUETES', payload: response.data.response })
+
       } catch (error) {
         console.log(error)
       }
     }
   },
   obtenerPaquetesPorCategoria: (categoria) => {
-    return  (dispatch, getState) => {
+    return (dispatch, getState) => {
       try {
-        dispatch({type: 'PAQUETES_CATEGORIA', payload: categoria})
+        dispatch({ type: 'PAQUETES_CATEGORIA', payload: categoria })
       } catch (error) {
         console.log(error)
       }
@@ -24,18 +25,18 @@ const paqueteActions = {
   obtenerPaquetePorId: (_id) => {
     return (dispatch, getState) => {
       try {
-        dispatch({type: 'PAQUETE_ID', payload: _id})
+        dispatch({ type: 'PAQUETE_ID', payload: _id })
         // return(getState().paqueteReducer.todosLosPaquetes.find(paquete => paquete._id  === _id))
       } catch (error) {
-        console.log(error) 
+        console.log(error)
       }
-      return(getState().paqueteReducer.paquetePorId)
+      return (getState().paqueteReducer.paquetePorId)
     }
   },
   filtrarPaquetesMasReg: () => {
     return (dispatch, getState) => {
       try {
-        dispatch({type: 'PAQUETES_MAS_REG', payload: getState})
+        dispatch({ type: 'PAQUETES_MAS_REG', payload: getState })
       } catch (error) {
         console.log(error)
       }
@@ -44,28 +45,80 @@ const paqueteActions = {
   filtrarPaquetes: (valor) => {
     return (dispatch, getState) => {
       try {
-        dispatch({type: 'FILTRO', payload: valor})
+        dispatch({ type: 'FILTRO', payload: valor })
       } catch (error) {
         console.log(error)
       }
     }
   },
-  obtenerValoracion:(paquete) =>{
-     return(dispatch,getState)=> {
-         dispatch({type:'PROMEDIO', payload:paquete })
-        } 
-  },
-  enviarValoracion: (_id,usuarioYvaloracion)=>{
-    return async (dispatch,getState)=>{
-      const response= await axios.put(`http://localhost:4000/api/paquetes/${_id}`,usuarioYvaloracion)
-      if(response.data.response){
-         console.log("VALORACION NUEVA QUE MANDO AL BACK")
-          console.log(response.data.response)
-          dispatch({type:"ENVIAR_VALORACION",payload:response.data.response})}
-        }
+  obtenerValoracion: (paquete) => {
+    return (dispatch, getState) => {
+      dispatch({ type: 'PROMEDIO', payload: paquete })
     }
+  },
+  enviarValoracion: (_id, usuarioYvaloracion) => {
+    return async (dispatch, getState) => {
+      const response = await axios.put(`http://localhost:4000/api/paquetes/${_id}`, usuarioYvaloracion)
+      if (response.data.response) {
+        console.log("VALORACION NUEVA QUE MANDO AL BACK")
+        console.log(response.data.response)
+        dispatch({ type: "ENVIAR_VALORACION", payload: response.data.response })
+      }
+    }
+  },
+  agregarComentario: nuevoComentario => {
+    const {comentario, token, paqueteId} = nuevoComentario      
+    return async(dispatch, getState) => {
+      try{
+        const response = await axios.post(`http://localhost:4000/api/paquete/comentario`, {comentario, paqueteId}, 
+        {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        dispatch({type: 'ENVIAR_COMENTARIO', payload: response.data.response})
+      }catch(error){
+        Swal.fire('ENTRE EN EL ERROR DE AGREGAR COMENTARIO')
+      }
+      
+    }
+},
+eliminarComentario: comentarioAEliminar => {
+    const { paqueteId, token, comentarioId } = comentarioAEliminar
+    return async (dispatch, getState) => {
+      try{
+        const response = await axios.delete(`http://localhost:4000/api/paquete/comentario/${paqueteId}/${comentarioId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      dispatch({ type: 'ELIMINAR_COMENTARIO', payload: response.data.response })
+      }catch(error){
+        Swal.fire('ENTRE EN EL ERROR DE ELIMINAR COMENTARIO')
+      }
+      
+    }
+  },
+  editarComentario: comentarioAEditar => {
+    const {comentarioId, paqueteId, comentarioEditado, token } = comentarioAEditar
+    return async (dispatch, getState) => {
+      try{
+        const response = await axios.put(`http://localhost:4000/api/paquete/comentario`, { comentarioId, paqueteId, comentarioEditado },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })     
+      dispatch({ type: 'EDITAR_COMENTARIO', payload: response.data.response })
+      }catch(error){
+        Swal.fire('ENTRE EN EL ERROR DE EDITAR COMENTARIO')
+      }
+    }
+  },
 }
-  
-    
+
+
 
 export default paqueteActions;
