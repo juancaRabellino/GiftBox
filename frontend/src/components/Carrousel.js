924f2d1aea4d3fc753cc3036fa93fa34266824a6
Jordi Belmonte <jorte.03@gmail.com>
2021-03-08 12:03:31 -0300

    carrusel

--- frontend/src/components/Carrousel.js
+++ frontend/src/components/Carrousel.js
@@ -7,12 +7,13 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 const fotos = [{ img: 'https://fotos.subefotos.com/56c90e1f86df75fcfc6a019320c6deefo.png', text: 'Aventura', color: '#FFC715' }, { img: 'https://fotos.subefotos.com/32bf744cf1edec87692e8e32b04b89d8o.jpg', text: 'Blends', color: '#30d42a' }, { img: 'https://fotos.subefotos.com/26d409700fdaa30b2a9c9d51283b0fd8o.jpg', text: 'En casa', color: '#CA360C' }, { img: 'https://fotos.subefotos.com/654fee5dbf75e34d3e4b9d362705b774o.jpg', text: 'Entretenimiento', color: '#C7BBEC' }, { img: 'https://fotos.subefotos.com/e606178ce6f8e48f545dc3b053324f59o.jpg', text: 'Estadias', color: '#43DCB7' }, { img: 'https://i.postimg.cc/DfR65MtV/Estar-bien.jpg', text: 'Estar bien', color: '#B6B6EF' }, { img: 'https://fotos.subefotos.com/75ba75aa9a87f616e495467ec2ec2746o.jpg', text: 'Gastronomia', color: '#FF4F6D' }]
+
 const Carrousel = ({ todasLasCategorias }) => {
-  var settings = {
+ var settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1
   };
   return (
